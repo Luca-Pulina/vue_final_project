0efc463d1c82e038cffdd2f982ae101d77e1ada4
lucaPulina <luca.pulina@4dem.it>
2019-06-26 17:02:51 +0200

    lezione 289

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="card" >
+      <div class="card-header bg-success">
+        <h3 class="panel-title">
+          NAME
+          <small>(Price: PRICE)</small>
+        </h3>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col">
+            <input type="number" class="form-control" placeholder="Quantity">
+          </div>
+          <div class="col">
+            <button class="btn btn-success">Buy</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/stocks/Stocks.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="row myStock">
+        <app-stock v-for="stock in stocks"></app-stock>
+    </div>
+</template>
+
+<script>
+    import Stock from './Stock'
+    export default {
+        data(){
+            return{
+                stocks: [
+                    {id: 1, name: 'BMW', price: 110},
+                    {id: 2, name: 'Google', price: 200},
+                    {id: 3, name: 'Apple', price: 250},
+                    {id: 4, name: 'Twitter', price: 8},
+                ]
+            }
+        },
+        components:{
+            appStock: Stock
+        }
+    }
+</script>
+
+<style>
+    .myStock{
+        margin-top: 40px;
+    }
+</style>
