5f5eb32f6970d999d1edc7640423861f6846b909
lucaPulina <luca.pulina@4dem.it>
2019-06-28 15:46:43 +0200

    finita crazione portfolio.js - lezione 291

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -1,5 +1,5 @@
 const state = {
-    founds: 10000,
+    funds: 10000,
     stocks: []
 }
 
@@ -14,24 +14,41 @@ const mutations = {
                 quantity: quantity
             })
         }
+        state.funds -= stockPrice * quantity
     },
     'SELL_STOCK'(state, {stockId, quantity, stockPrice}){
         const record = state.stocks.find(element => element.id == stockId)
         if(record.quantity > quantity){
             record.quantity -= quantity
         }else{
-            state.stocks.spilce({ })
-              
+            state.stocks.spilce(state.stocks.indexOf(record), 1)  
         }
+        state.funds += stockPrice * quantity
     }
 }
 
 const actions = {
+    sellStock({commit}, order){
+        commit('SELL_STOCK', order)
+    }
 
 }
 
 const getters = {
-
+    stockPortfolio(state, getters){
+        return state.stocks.map(stocks => {
+            const record = getters.stocks.find(element => element.id == stock.id)
+            return {
+                id: stock.id,
+                quantity: stock.quantity,
+                name: record.name,
+                price: record.price
+            }
+        })
+    },
+    funds(state){
+        return state.funds
+    }
 
 }
 
